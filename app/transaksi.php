965f55a8b81205592f1db6207df0ba832e6bb0f6
danilmuis <daniltelkom@gmail.com>
2019-11-29 20:09:14 +0700

    test transaksi simpan

--- app/transaksi.php
+++ app/transaksi.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class transaksi extends Model
 {
+    private $debit=50000;
     protected $table = 'transaksi';
     protected $fillable = ['noTransaksi',  'jumlahUang', 'jenisTransaksi','mediaPembayaran', 'idAnggota','idTujuan'];
     public $timestamps = false;
+
+    public function simpan($uang){
+        return $this->debit + $uang;
+    }
 }
